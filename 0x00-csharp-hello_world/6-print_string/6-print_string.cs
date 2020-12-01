708f3563c1a65cf5a86e4e508e2f5484264b5aa7
Dev-Loup <60360505+Dev-Loup@users.noreply.github.com>
2020-12-01 12:44:44 -0500

    [add]: using loops

---
+++ 0x00-csharp-hello_world/6-print_string/6-print_string.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+class Program
+{
+	static void Main(string[] args)
+	{
+		string str = "Holberton School";
+        int iter = 0;
+		for(iter = 1;iter <=3; iter++)
+        {
+            Console.Write("{0}", str);
+        }
+        Console.Write("\n{0}\n", str.Substring(0,9));
+        }
+}
