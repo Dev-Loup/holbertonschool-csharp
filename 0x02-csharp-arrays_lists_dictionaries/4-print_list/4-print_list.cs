d16104c9fbe3b9a3b84e70ea02fe9d0c1968b0de
Dev-Loup <60360505+Dev-Loup@users.noreply.github.com>
2020-12-03 16:59:34 -0500

    [add]: set a list type collection data

---
+++ 0x02-csharp-arrays_lists_dictionaries/4-print_list/4-print_list.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+
+class List
+{
+    public static List<int> CreatePrint(int size)
+    {
+        if (size < 0)
+        {
+            Console.WriteLine("Size cannot be negative");
+            return null;
+        }
+        List<int> num_list = new List<int>();
+		for (int iter = 0; iter < size; iter++)
+		{
+			num_list.Add(iter);
+			Console.Write(iter);
+			if (iter < size - 1)
+				Console.Write(' ');
+		}
+		Console.WriteLine();
+		return num_list;
+    }
+}
