4e33fa50fd68e7f5d011b98588b39a92de83d5d7
Dev-Loup <60360505+Dev-Loup@users.noreply.github.com>
2020-12-03 14:02:09 -0500

    [add]: check index

---
+++ 0x02-csharp-arrays_lists_dictionaries/1-element_at/1-element_at.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+class Array
+{
+    public static int elementAt(int[] array, int index)
+    {
+        if (array.Length > index && index > 0)
+        {
+            return array[index];
+        }
+        return -1;
+    }
+}
