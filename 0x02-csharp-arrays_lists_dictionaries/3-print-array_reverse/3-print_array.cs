dd91f27f9b1d369e114d537322bb9b4bf621c65b
Dev-Loup <60360505+Dev-Loup@users.noreply.github.com>
2020-12-03 16:12:47 -0500

    [add]: reversed printing

---
+++ 0x02-csharp-arrays_lists_dictionaries/3-print-array_reverse/3-print_array.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+class Array
+{
+    public static void Reverse(int[] array)
+    {
+        if (array != null)
+        {
+            for (int iter = array.Length - 1; iter >= 0; iter--)
+            {
+                if (iter != (array.Length - 1))
+                {
+                    Console.Write(" ");
+                }
+                Console.Write("{0}", array[iter]);
+            }
+        }
+        Console.WriteLine();
+    }
+}
