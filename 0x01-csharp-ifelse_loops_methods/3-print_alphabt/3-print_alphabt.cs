82f1ba02113c30cb10e1b99a0241947795b49e65
Dev-Loup <60360505+Dev-Loup@users.noreply.github.com>
2020-12-02 22:01:14 -0500

    [add]: print alphabet without q & e

---
+++ 0x01-csharp-ifelse_loops_methods/3-print_alphabt/3-print_alphabt.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace _3_print_alphabt
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            for (char letter = 'a'; letter <= 'z'; letter++)
+            {
+                if (letter != 'q' && letter != 'e')
+                {
+                    Console.Write("{0}", letter);
+                }
+            }
+        }
+    }
+}
