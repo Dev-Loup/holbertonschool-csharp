a830dd546da3e28cf264ed79a3485e864cb8720a
Dev-Loup <60360505+Dev-Loup@users.noreply.github.com>
2020-12-03 14:39:35 -0500

    [add]: replace element at index

---
+++ 0x02-csharp-arrays_lists_dictionaries/2-replace_element/2-replace_element.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+class Array
+{
+    public static int[] ReplaceElement(int[] array, int index, int n)
+    {
+        if (array.Length > index && index >= 0)
+        {
+            array[index] = n;
+            return array;
+        }
+        Console.WriteLine("Index out of range");
+        return array;
+    }
+}
