a63492f809d8edc7807565db9a9904f232e0d170
Dev-Loup <60360505+Dev-Loup@users.noreply.github.com>
2020-12-03 17:31:50 -0500

    [add]: return a list of booleans reporting 2 divisibility

---
+++ 0x02-csharp-arrays_lists_dictionaries/6-divisible_by_2/6-divisible_by_2.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+
+class List
+{
+    public static List<bool> DivisibleBy2(List<int> myList)
+    {
+        List<bool> report = new List<bool>();
+        for (int i = 0; i < myList.Count; i++)
+        {
+            report.Add(myList[i] % 2 == 0);
+        }
+        return report;
+    }
+}
