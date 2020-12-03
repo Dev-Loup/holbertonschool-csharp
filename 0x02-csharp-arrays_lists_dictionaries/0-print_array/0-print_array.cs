9251902391fd98e1d7a65b7333d98a1fa1320f2b
Dev-Loup <60360505+Dev-Loup@users.noreply.github.com>
2020-12-03 13:38:21 -0500

    [add]: print array of ints by size

---
+++ 0x02-csharp-arrays_lists_dictionaries/0-print_array/0-print_array.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+class Array
+{
+    public static int[] CreatePrint(int size)
+    {
+        if (size < 0)
+        {
+            Console.WriteLine("Size cannot be negative");
+            return null;
+        }
+        int[] num_array = new int[size];
+        for(int iter = 0; iter < size; iter++)
+        {
+            num_array[iter] = iter;
+            if (iter != 0)
+            {
+                Console.Write(" ");
+            }
+            Console.Write("{0}", iter);
+        }
+        Console.WriteLine();
+        return num_array;
+    }
+}
