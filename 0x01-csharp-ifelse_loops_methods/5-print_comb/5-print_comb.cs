56b761a21130acf4142e1ccf8cec058e13415afc
Dev-Loup <60360505+Dev-Loup@users.noreply.github.com>
2020-12-02 22:33:09 -0500

    [add]: from zero to 99 list

---
+++ 0x01-csharp-ifelse_loops_methods/5-print_comb/5-print_comb.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace _5_print_comb
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            for (int num = 0; num <= 99; num++)
+            {
+                if (num != 99)
+                {
+                    Console.Write("{0:00}, ", num);
+                }
+                else
+                {
+                    Console.WriteLine("{0:0}", num);
+                }
+            }
+        }
+    }
+}
