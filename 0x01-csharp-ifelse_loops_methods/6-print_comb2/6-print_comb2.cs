64e89e7d352853f74c139b881001a4245d997c21
Dev-Loup <60360505+Dev-Loup@users.noreply.github.com>
2020-12-02 22:43:05 -0500

    [add]: unique two digits numbers

---
+++ 0x01-csharp-ifelse_loops_methods/6-print_comb2/6-print_comb2.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace _6_print_comb2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            for (int tens = 0; tens < 10; tens++)
+            {
+                for (int units = 0; units < 10; units++)
+                {
+                    if (tens == 8 && units == 9)
+                    {
+                        Console.WriteLine("{0}{1}",tens, units);
+                    }
+                    else if (units < tens)
+                    {
+                        Console.Write("{0}{1}, ",tens, units);
+                    }
+                }
+            }
+        }
+    }
+}
